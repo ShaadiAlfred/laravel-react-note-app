d937dda74cc73be747a3ed0b5dd424f0dbbb8452
Shaadi Alfred <shaadi.alfred@gmail.com>
2021-07-20 00:29:05 +0200

    Axios withCredentials

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import axios from "axios";
 
 import App from "./Components/App";
+
+axios.defaults.withCredentials = true;
 
 render(
     <Provider store={store}>
